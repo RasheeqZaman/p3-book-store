9ba3534170175e589492ae9de9b5a945a40912ea
RasheeqZaman <jbc.rasheeq@gmail.com>
2026-02-03 15:42:34 +0600

    Refactor HelloWorldController to include user and admin greeting endpoints

--- src/main/java/com/wolftech/bookstore/HelloWorldController.java
+++ src/main/java/com/wolftech/bookstore/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.wolftech.bookstore;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController @RequestMapping("hello")
 public class HelloWorldController {
-    @GetMapping("/hello")
-    String say() {
-        return "Hello, World!";
+
+    @GetMapping("user")
+    public String helloUser() {
+        return "Hello User";
+    }
+
+    @GetMapping("admin")
+    public String helloAdmin() {
+        return "Hello Admin";
     }
 }
